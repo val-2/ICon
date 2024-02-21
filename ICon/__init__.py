009ec9b728f972be4db59ab45398bdd22055992e
val-2 <programming.recantingly@aleeas.com>
2024-02-21 19:54:57 +0100

    Update __init__.py

--- ICon/__init__.py
+++ ICon/__init__.py
@@ -5,7 +5,7 @@ import pathlib
 df = pd.read_csv(pathlib.Path(__file__).parent.parent / 'used_cars.csv')
 
 # Pre-processing
-df = df.drop('Service history', axis=1)
+# df = df.drop('Service history', axis=1)
 
 
 # Print the DataFrame
