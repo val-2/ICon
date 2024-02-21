eefd2d3def5924e721fe3d1dd7ca1276e97d838a
val-2 <programming.recantingly@aleeas.com>
2024-02-21 19:41:57 +0100

    Create __init__.py

---
+++ ICon/__init__.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pathlib
+
+# Read the CSV file
+df = pd.read_csv(pathlib.Path(__file__).parent.parent / 'used_cars.csv')
+
+# Pre-processing
+df = df.drop('Service history', axis=1)
+
+
+# Print the DataFrame
+print(df)
